a99807654083a76ae96dffb817f6a61f005f4d9d
dtanphat9388 <dtanphat9388@gmail.com>
2025-01-31 05:21:37 +0700

    fixed yq, docker

--- yq/install.sh
+++ yq/install.sh
@@ -2,16 +2,4 @@
 
 # description: yaml parser
 
-yq_brew() {
-  brew install yq
-}
-
-yq_snap() {
-  snap install yq
-}
-
-if [[ $DF_IS_HOMEBREW ]]; then
-  yq_brew
-elif [[ $DF_IS_SNAP ]]; then
-  yq_snap
-fi
+brew install yq
--- yq/uninstall.sh
+++ yq/uninstall.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-app_brew_uninstall() {
-  return
-}
-
-app_apt_uninstall() {
-  return
-}
-
-if [[ $DF_IS_HOMEBREW ]]; then
-  app_brew_uninstall
-elif [[ $DF_IS_APT ]]; then
-  app_apt_uninstall
-fi
+brew uninstall yq
