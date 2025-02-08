5406f88bc3fd64224ead3f9b2a5ece8b24126532
dtanphat9388 <dtanphat9388@gmail.com>
2025-02-09 03:44:34 +0700

    saved

--- keyd/install.sh
+++ keyd/install.sh
@@ -6,8 +6,4 @@ if [[ $DF_IS_LINUX ]]; then
   cd $clonefolder
   make && sudo make install
   sudo systemctl enable keyd && sudo systemctl start keyd
-
 fi
-
-sudo ln -sfn $DF_CONFIGS/keyd/app.conf $HOME/.config/keyd/app.conf
-sudo ln -sfn $DF_CONFIGS/keyd/default.conf /etc/keyd/default.conf
