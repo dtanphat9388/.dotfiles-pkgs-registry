3622547567710268060c43ddb8e6d2ba901b6747
dtanphat9388 <dtanphat9388@gmail.com>
2025-05-16 18:01:55 +0700

    fix asdf

--- asdf/env.sh
+++ asdf/env.sh
@@ -3,11 +3,8 @@
 # export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
 #-- source exe file
-if [[ $DF_IS_LINUX ]]; then
-  ASDF_EXE_PATH="$(brew --prefix asdf)/libexec/asdf.sh"
-  [[ ! "$ASDF_EXE_PATH" ]] && return
-  . "$ASDF_EXE_PATH"
-fi
+ASDF_EXE_PATH="$(brew --prefix asdf)/libexec/asdf.sh"
+[[ ! "$ASDF_EXE_PATH" ]] && return || . "$ASDF_EXE_PATH"
 
 export ASDF_CONFIG_FILE=$DF_CONFIGS/asdf/.asdfrc
 
