1eb80359ffc0d3671246510fde01402ecaa70092
dtanphat9388 <dtanphat9388@gmail.com>
2025-07-11 08:04:28 +0700

    saved from macos

--- espanso/ln.sh
+++ espanso/ln.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-if [[ -L $HOME/.config/espanso ]]; then
-  ln -sfn $DF_CONFIGS/espanso $HOME/.config
+if [[ $DF_IS_MACOS && $DF_IS_ARCH_INTEL ]]; then
+  if [[ ! -L $HOME/.config/espanso ]]; then
+    ln -sfn $DF_CONFIGS/espanso "$HOME/Library/Application Support"
+  fi
+fi
+
+if [[ $DF_IS_LINUX ]]; then
+  if [[ ! -L $HOME/.config/espanso ]]; then
+    ln -sfn $DF_CONFIGS/espanso $HOME/.config
+  fi
 fi
